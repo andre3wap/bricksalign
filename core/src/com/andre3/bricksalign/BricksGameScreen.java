445614325af572d3001a20613e976bc67c1a5aef
Andre Brown <andre.brown@oit.nj.giv>
2016-07-29 10:18:59 -0400

    test

--- core/src/com/andre3/bricksalign/BricksGameScreen.java
+++ core/src/com/andre3/bricksalign/BricksGameScreen.java
@@ -1,34 +1,27 @@
 package com.andre3.bricksalign;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 
 /**
  * Created by ODBBROW on 7/29/2016.
  */
-public class BricksMenuScreen implements Screen {
+public class BricksGameScreen implements Screen {
 
-    final BricksAlign game;
+    BricksAlign game;
     OrthographicCamera camera;
 
-    public BricksMenuScreen(final BricksAlign game) {
+    public BricksGameScreen(BricksAlign game) {
         this.game = game;
 
         camera = new OrthographicCamera();
-        camera.setToOrtho(false, 800, 480);
+        camera.setToOrtho(false, 400, 480);
     }
 
     @Override
     public void render(float delta) {
-
         camera.update();
         game.batch.setProjectionMatrix(camera.combined);
-
-        if (Gdx.input.isTouched()) {
-            game.setScreen(new BricksGameScreen(game));
-            dispose();
-        }
 
 
     }
