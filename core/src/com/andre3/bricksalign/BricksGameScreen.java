e14aa425473d264a3bafb5fc29fd7cdfbe6f3d9b
Andre Brown <andre.brown@oit.nj.giv>
2016-07-29 11:58:24 -0400

    test

--- core/src/com/andre3/bricksalign/BricksGameScreen.java
+++ core/src/com/andre3/bricksalign/BricksGameScreen.java
@@ -1,11 +1,16 @@
 package com.andre3.bricksalign;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.TimeUtils;
+
+import java.util.Iterator;
 
 import javax.sound.midi.SysexMessage;
 
@@ -17,7 +22,8 @@ public class BricksGameScreen implements Screen {
     BricksAlign game;
     OrthographicCamera camera;
     ShapeRenderer shape;
-    Array<ShapeRenderer> shapes;
+    Array<Rectangle> shapes;
+    float lastDropTime;
 
     public BricksGameScreen(BricksAlign game) {
         this.game = game;
@@ -25,6 +31,10 @@ public class BricksGameScreen implements Screen {
         shape = new ShapeRenderer();
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 400, 480);
+
+        shapes = new Array<Rectangle>();
+        spawnShape();
+
     }
 
     @Override
@@ -36,17 +46,33 @@ public class BricksGameScreen implements Screen {
         shape.begin(ShapeRenderer.ShapeType.Filled);
 
          float x = MathUtils.random(0, 800-64);
+        for(Rectangle square : shapes) {
 
-
-        shape.rect(100, x, 100, 100);
+            shape.rect(square.x, square.y, square.width, square.height);
+        }
         shape.end();
+
+        if(TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnShape();
+
+        Iterator<Rectangle> iter = shapes.iterator();
+        while(iter.hasNext()) {
+            Rectangle shape = iter.next();
+            shape.y -= 200 * Gdx.graphics.getDeltaTime();
+            if(shape.y + 64 < 0) iter.remove();
+
+        }
+
     }
 
     public void spawnShape()
     {
-
-
-
+        Rectangle square = new Rectangle();
+        square.x = MathUtils.random(0, 800-64);
+        square.y = 480;
+        square.width = 100;
+        square.height = 100;
+        shapes.add(square);
+        lastDropTime = TimeUtils.nanoTime();
     }
 
     @Override
